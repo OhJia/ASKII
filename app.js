009634b03a015c9c1df6054bd34a90537344022c
OhJia <jiajia0919@gmail.com>
2015-05-03 20:02:40 -0400

    first comment

---
+++ app.js
@@ -0,0 +1,124 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+
+var mongo = require('mongo');
+var mongoose = require('mongoose');
+var mongoUri = "mongodb://heroku_app35895981:uouj39lc2h4qlphv8e2n8jes58@ds031892.mongolab.com:31892/heroku_app35895981";
+//var mongoUri = mongoUri.formatMongoose(url);
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
+db.on('error', function(error){
+	console.log(error);
+});
+db.on('open', function(){
+	console.log('SUCCESS! Connected to Mongo');
+});
+
+var qText, qLoc, qYes, qNo, qComm; 
+
+// QUESTION COLLECTION
+var questionSchema = mongoose.Schema({
+  text: String,
+  location: String,
+  yes: int,
+  no: int, 
+  comments: int  
+});
+
+var Question = mongoose.model('questions', questionSchema); // questions doesn't matter, just collection name in mongo
+
+// COMMENT COLLECTION
+var commentSchema = mongoose.Schema({
+
+});
+
+var Comment = mongoose.model('comments', commentSchema);
+
+// USER COLLECTION
+var userSchema = mongoose.Schema({
+
+});
+
+var User = mongoose.model('users', userSchema);
+
+
+
+
+// var app = express.createServer();
+var app = express();
+app.use(bodyParser());
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/questions', function(req, res){
+   var questions = [
+     {id: 1, title: 'What is up?'},
+     {id: 2, title: 'Is it fun?'},
+     {id: 3, title: 'Hello'}
+   ];
+   res.send(questions);
+
+   ////////////// show questions from mongoDB
+   questions.find()
+    .exec(function(err, questions){
+      if (err){
+        res.status(500).json({status: 'failure'});
+      } else {
+        // res.render('questions', {
+        //   text: questions.text,
+        //   comments: questions.comments
+        // });
+        res.send(success: true);
+      }
+    });
+    res.send(questions);
+
+});
+
+app.post('/new-question', function(req, res){
+  qText = req.body.text;
+  qLoc = req.body.location;
+  qYes = req.body.yes;
+  qNo = req.body.no;
+  qComm = req.body.comments;
+  
+  var q = new Question(
+    questions[text] = qText;
+    questions[location]= qLoc;
+    questions[yes] = qYes;
+    questions[no] = qNo;
+    questions[comments] = qComm;
+  );
+
+  q.save(function(err){
+    console.log(err);
+  });
+});
+
+// app.get('/', function(req, res){
+//   res.render('index.ejs', {title: 'Clever Kitchens'});
+// });
+
+// app.get('/recipes', function(req, res){
+//   res.render('layout.ejs', {
+//     title: 'Clever Kitchens - Recipes', 
+//     body: '<h1>All Recipes</h1>'
+//   });
+// });
+
+// app.get('/recipes/:title', function(req, res) {
+//  res.send('<h1>' + req.params.title + '</h1>');
+// });
+
+app.get('/*', function(req, res) {
+  res.status(404).render('error.ejs', {title: 'Error'});
+});
+
+// app.listen(3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
